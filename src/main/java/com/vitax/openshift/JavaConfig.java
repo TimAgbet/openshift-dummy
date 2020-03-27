a262b2962f7efd2676e3346c00bb2b28fee0009f
glamma <glammaman@hotmail.com>
2020-03-27 11:53:49 +0000

    Start of spring boot project

---
+++ src/main/java/com/vitax/openshift/JavaConfig.java
@@ -0,0 +1,12 @@
+package com.vitax.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConfig {
+
+    public static void main(String[] args){
+        SpringApplication.run(JavaConfig.class, args);
+    }
+}
